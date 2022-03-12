b9e93c947e552065989d19b936fce31dafa985ba
sakoji14 <sj.yu.sato1424@gmail.com>
2022-03-12 21:06:18 +0900

    ログイン、ログアウト機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,10 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
+  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :second_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :second_name_kana, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :birthday, presence: true
+
 end
