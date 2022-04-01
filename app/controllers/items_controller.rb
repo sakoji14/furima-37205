8dcf818befc40a1972f26070b168b0e4611c6451
sakoji14 <sj.yu.sato1424@gmail.com>
2022-04-01 12:48:48 +0900

    訂正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,7 +19,9 @@ class ItemsController < ApplicationController
   def destroy
     item = Item.find(params[:id])
     item.destroy
-    redirect_to root_path
+    if @item.user == current_user
+      redirect_to root_path
+    end
   end
 
   def create
